2b819a2345fa9dcdef3d9b3f916bad9795725f71
Adam Lengyel <lengyel.adam95@gmail.com>
2018-02-26 17:23:50 +0100

    Database and upload method created

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/clothes-webshop');
 
+var Product = require('./model/product');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -10,6 +12,20 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req, res) =>{
     res.send("GET request to the homepage");
+});
+
+app.post('/product', (req, res) =>{
+    var product = new Product(req.body);
+    
+    
+    
+    product.save((err, savedProduct) =>{
+       if (err) {
+           res.status(500).send({error:"Could not save product"});
+       } else {
+           res.status(200).send(savedProduct);
+       }
+    });
 });
 
 
