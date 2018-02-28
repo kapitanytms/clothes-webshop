384bafe0014859860a3990e753b95aa283f81e9a
Adam Lengyel <lengyel.adam95@gmail.com>
2018-02-28 19:26:41 +0100

    GET method for fetching products added

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("GET request to the homepage");
 });
 
-app.post('/product', (req, res) =>{
+app.post('/product', (req, res) => {
     var product = new Product(req.body);
     
     
     
-    product.save((err, savedProduct) =>{
+    product.save((err, savedProduct) => {
        if (err) {
            res.status(500).send({error:"Could not save product"});
        } else {
            res.status(200).send(savedProduct);
        }
+    });
+});
+
+
+app.get('/product', (req, res) => {
+    Product.find({}, (err, products) => {
+        if (err){
+            res.status(500).send({error:"Could not fetch products"});
+        } else {
+            res.status(200).send(products);
+        }
     });
 });
 
