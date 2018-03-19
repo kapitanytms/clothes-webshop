44a0b97ea87f97866ff0dcf83b42f56022e61aff
Adam Lengyel <lengyel.adam95@gmail.com>
2018-03-19 18:12:58 +0100

    Product upload bugs fixed

--- backend/model/product.js
+++ backend/model/product.js
@@ -2,15 +2,42 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var product = new Schema({
-    name: String,
-    size: [String],
-    price: Number,
-    quantity: Number,
-    color: [String],
-    imgPaths: [String],
-    gender: String,
-    category: String,
-    adult: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name is required.']
+    },
+    size: {
+        type: [String],
+        required: [true, 'Size is required.']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required.']
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Quantity is required.']
+    },
+    color: {
+        type: [String],
+        required: [true, 'Color is required.']
+    },
+    imgPaths: {
+        type: [String],
+        required: [true, 'Image paths are required.']
+    },
+    gender: {
+        type: String,
+        required: [true, 'Gender is required.']
+    },
+    category: {
+        type: String,
+        required: [true, 'Category is required.']
+    },
+    adult: {
+        type: Boolean,
+        required: [true, 'Adult is required.']
+    }
 });
 
 module.exports = mongoose.model('Product', product);
